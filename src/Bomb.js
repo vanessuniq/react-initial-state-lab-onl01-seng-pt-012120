3d7eee52fd803150ac2c16ea189424ac3a48a25e
vanessuniq <vanessuniq@gmail.com>
2020-10-04 23:31:58 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {secondsLeft: this.props.initialCount}
+    }
+    countDown() {
+        setTimeout(() => {
+            this.setState({secondsLeft: this.state.secondsLeft -1})
+        }, 1000);
+    }
+    render() {
+        this.countDown()
+        return (
+            <div>
+                {this.state.secondsLeft === 0? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}
+            </div>
+        );
+    }
+}
+
+export default Bomb;
+
